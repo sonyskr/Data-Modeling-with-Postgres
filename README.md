11fd98875535d56bd4fc12daa06caf144449c2c9
Youngsik Son <youngsik.son@gmail.com>
2021-10-10 18:42:58 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Data-Modeling-with-Postgres
+# Project: Data Modeling with Postgres
+
+------------------------------------------
+### Purpose of database
+A startup called Sparkify wants to analyze the data they've been collecting on songs and user activity on their new music streaming app. The analytics team is particularly interested in understanding what songs users are listening to. Currently, they don't have an easy way to query their data, which resides in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+As a data engineer, I need to create a database schema and ETL pipeline for this analysis so that analytics team can easily query the data they require.
+
+--------------------------------------------
+
+#### Fact Table 
+1. **songplays** - records in log data associated with song plays i.e. records with page `NextSong` 
+    + *songplay_id (PK), start_time , user_id , level, song_id , artist_id , session_id, location, user_agent*
+
+#### Dimension Tables 
+2. **users** - users in the app 
+    + *user_id (PK), first_name, last_name, gender, level*
+
+3. **songs** - songs in music database
+    + *song_id (PK), title, artist_id, year, duration*
+
+4. **artists** - artists in music database
+    + *artist_id (PK), name, location, lattitude, longitude*
+
+5. **time** - timestamps of records 
+    + *start_time (PK), hour, day, week, month, year, weekday*
+
+--------------------------------------------
+### Project Structure 
+In addition to the data files, the project includes six files:
++ `test.ipynb` - This notebook is used to check on the database;
++ `create_tables.py` - This script will drop and create the tables;
++ `etl.ipynb` - This notebook reads and processes files from data and loads them into the tables;
++ `etl.py` - This script is our ETL script which will allow us to read the data from JSON files , parse them and insert into the corresponded table;
++ `sql_queries.py` - This script contains all your sql queries, and is imported into the last three files above.
+
+--------------------------------------------
+### Running procedure
+
+1. Run `create_tables.py` from the Terminal to create the 'sparkifydb' database and tables.
+2. Run 'etl.ipynb' to get data from files and then insert the data into tables.
+3. Run test.ipynb to confirm your records were successfully inserted into each table.
+
---
+++ etl.py
@@ -0,0 +1,135 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+"""
+    This procedure processes a song file whose filepath has been provided as an arugment.
+    It extracts the song information in order to store it into the songs table.
+    Then it extracts the artist information in order to store it into the artists table.
+
+    INPUTS: 
+    * cur the cursor variable
+    * filepath the file path to the song file
+    
+    RETURNS: none
+"""
+
+def process_song_file(cur, filepath):
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values[0].tolist()
+    cur.execute(song_table_insert, song_data)
+    
+    # insert artist record
+    artist_data = df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values[0].tolist()
+    cur.execute(artist_table_insert, artist_data)
+
+"""
+    This procedure processes a log file whose filepath has been provided as an arugment.
+    It extracts the start_time information in order to store it into the time table.
+    Then it extracts the user information in order to store it into the user table.
+    At last, since the log file does not specify an ID for either the song or the artist, we extract the song ID and artist ID by querying the songs and artists tables to find matches based on song title, artist name, and song duration time to store it into songplay table.
+    
+    INPUTS: 
+    * cur the cursor variable
+    * filepath the file path to the song file
+    
+    RETURNS: none
+"""
+def process_log_file(cur, filepath):
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df[df['page']=='NextSong']
+
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df['ts'], unit='ms')
+    
+    # insert time data records
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame.from_dict({c : t for c, t in zip(column_labels, time_data)})
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+            print(results)
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
+
+'''
+    This procedure extracts all JSON files under the given filepath and process each of those file contents with the given functions.
+    
+     INPUTS: 
+    * cur the cursor variable
+    * conn active open connection on sparkify database
+    * filepath the file path to the song file
+    * func function name which needs to be invoked while processing each file under the given filepath
+    
+    RETURNS: none
+    '''
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+'''
+    This is the main function to process song and log datasets.
+    
+    Connects to sparkify database and the cursor is opened for data processing.
+    Invokes functions to process song and log datasets in order to load the 4 dimension tables (songs, artists, users, time) and 1 fact table (songplays)
+    
+    INPUTS: none
+    RETURNS: none
+'''        
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
